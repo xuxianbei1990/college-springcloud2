f97a11641ba97a8d7cefd483030284dfa9176223
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2021-08-17 17:56:44 +0800

    自测时间导出

--- college-service-common/src/main/java/college/springcloud/common/utils/excel/ExcelExportCustomService.java
+++ college-service-common/src/main/java/college/springcloud/common/utils/excel/ExcelExportCustomService.java
@@ -2,6 +2,7 @@ package college.springcloud.common.utils.excel;
 
 import cn.afterturn.easypoi.excel.entity.params.ExcelExportEntity;
 import cn.afterturn.easypoi.excel.export.ExcelExportService;
+import college.springcloud.common.utils.DateTimeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.poi.ss.usermodel.Cell;
@@ -9,6 +10,8 @@ import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -24,14 +27,20 @@ public class ExcelExportCustomService extends ExcelExportService {
     @Override
     public void createStringCell(Row row, int index, String text, CellStyle style, ExcelExportEntity entity) {
         super.createStringCell(row, index, text, style, entity);
-        if (StringUtils.isNotBlank(entity.getFormat())) {
+        if (entity != null && StringUtils.isNotBlank(entity.getFormat())) {
             Cell cell = row.getCell(index);
             try {
-                Date date = DateUtils.parseDate(text, entity.getFormat());
+                Date date = null;
+                if (entity.getMethod().getReturnType().equals(LocalDateTime.class) ||
+                        entity.getMethod().getReturnType().equals(LocalDate.class)) {
+                    date = DateTimeUtils.localDateTimeToDate(LocalDateTime.parse(text));
+                } else if (entity.getMethod().getReturnType().equals(Date.class)) {
+                    date = DateUtils.parseDate(text, entity.getFormat());
+                }
                 style.setDataFormat(row.getSheet().getWorkbook().getCreationHelper().createDataFormat().getFormat(entity.getFormat()));
                 cell.setCellStyle(style);
                 cell.setCellValue(date);
-            } catch (ParseException e) {
+            } catch (Exception e) {
             }
         }
     }
