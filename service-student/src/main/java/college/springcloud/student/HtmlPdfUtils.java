6d21ad6ddd0490ae0dd9969d19072117d6bf5f2f
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2021-09-07 19:46:48 +0800

    feature:新增html转换PDF，字体

--- service-student/src/main/java/college/springcloud/student/HtmlPdfUtils.java
+++ service-student/src/main/java/college/springcloud/student/HtmlPdfUtils.java
@@ -1,6 +1,18 @@
 package college.springcloud.student;
 
+
+import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
+import com.itextpdf.kernel.counter.event.IMetaInfo;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.DocumentProperties;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.font.FontProvider;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.BaseFont;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.*;
@@ -18,10 +30,25 @@ public class HtmlPdfUtils {
     private static final String OUTPUT_FOLDER = "E:\\整理\\Java";
 
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
+        final ConverterProperties converterProperties = defaultFont();
         File htmlSource = new File(OUTPUT_FOLDER +"\\CHIN-EOP.html");
         File pdfDest = new File(OUTPUT_FOLDER + "\\output.pdf");
-        HtmlConverter.convertToPdf(new FileInputStream(htmlSource), new FileOutputStream(pdfDest));
+        HtmlConverter.convertToPdf(new FileInputStream(htmlSource), new FileOutputStream(pdfDest), converterProperties);
     }
+
+
+
+
+    private static ConverterProperties defaultFont() {
+        final ConverterProperties converterProperties = new ConverterProperties();
+        final FontProvider fontProvider = new DefaultFontProvider();
+        fontProvider.addDirectory("E:\\study\\GitHub\\college-springcloud2\\service-student\\out\\production\\resources\\font");
+//        final int ret = fontProvider.addSystemFonts();
+        converterProperties.setFontProvider(fontProvider);
+        return converterProperties;
+    }
+
+
+
 }
